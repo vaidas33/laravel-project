12b44f22e29a4754ec05f1850fe317383321d925
vaidas33 <73877931+vaidas33@users.noreply.github.com>
2022-10-27 02:52:15 +0300

    created a static page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,7 +5,7 @@
 <header class="py-5 bg-light border-bottom mb-4">
     <div class="container">
         <div class="text-center my-5">
-            <h1 class="fw-bolder">About us</h1>
+            <h1 class="fw-bolder">Contact us</h1>
         </div>
     </div>
 </header>
@@ -14,7 +14,7 @@
     <div class="row">
         <!-- Blog entries-->
         <div class="col-lg-12">
-            <p class="lead mb-0">About us text</p>
+            <p class="lead mb-0">Contact us text</p>
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::view('/', 'index')->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/contact', 'contact')->name('contact');
