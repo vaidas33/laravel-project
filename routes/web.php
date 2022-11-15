1af7d2153b974c9da7ae5014abd1b70ae1963da4
vaidas33 <73877931+vaidas33@users.noreply.github.com>
2022-11-15 18:50:43 +0200

    Post, controller, link

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'index')->name('home');
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::view('/about', 'about')->name('about');
 Route::view('/contact', 'contact')->name('contact');
+
+
+Route::get('posts/{post}', 
+[\App\Http\Controllers\PostController::class, 'show'])->name('posts.show');
